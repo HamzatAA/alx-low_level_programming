3ab16fe4c3d64837426c7fdca927262046c96b9a
HamzatAA <adeleayomide051@gmail.com>
2023-05-18 18:19:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
+/**
+* main - print program.
+* Return: 1 if success.
+*/
 
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 56);
 	return(1);
 }
