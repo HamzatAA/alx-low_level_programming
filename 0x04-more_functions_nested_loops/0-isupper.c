cac57c69c8730b8dad439027a147875680a86c69
HamzatAA <adeleayomide051@gmail.com>
2023-05-25 14:42:50 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks for uppercase character.
+ * Return: 1 if uppercase
+ */
+
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= '')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains the solutions to the 0x04-more_functions_nested_loops task
